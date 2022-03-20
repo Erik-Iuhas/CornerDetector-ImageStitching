b09406317dc6fd3fb07e482dcf24b07fea0529b5
Erik-Iuhas <erikiuhas@cmail.carleton.ca>
2022-03-20 11:56:39 -0400

    Comments Added

--- corner_detection/corner_detection.py
+++ corner_detection/corner_detection.py
@@ -1,22 +1,43 @@
 from tkinter.tix import WINDOW
 import cv2
 import numpy as np
+from sympy import true
+from copy import deepcopy
 
 class coner():
+    """This class is responsible for handling the global variables for the images so it can easily be accessed through class methods. 
+    """
     def __init__(self) -> None:
-        #Set Image Name 
+        """This method is responsible for loading the image and setting initial variables.
+        """
+        # Set file name for image
         self.image_name = "box_in_scene.png"
-        #Read Image
+        # Read in image as a grayscale
         self.orig_img = cv2.imread(self.image_name,cv2.IMREAD_GRAYSCALE)
-        #Display Original Image
+        # Display the un altered image
         cv2.imshow("Original Image", self.orig_img)
         
+        # Set initial value of slider to 100 which is then divided to equal 0.01
         self.INITIAL_SLIDER = 100
+        # The max value of the slider can only go up to 0.02
         self.MAX_SLIDER = 200
+        # Sets the window size for minimum eigen values and non max supression
         self.WINDOW_SIZE = 5
+        
+        self.save_image_once = True
     
 
     def binary_threshold(self,image,threshold):
+        """Threshold pixel values to zero if they are less than the threshold
+        and set pixels to 255 if they are greater. 
+
+        Args:
+            image (numpy array): A numpy array which stores the image
+            threshold (float): A float value which dictates the threshold 
+
+        Returns:
+            (numpy array): Retruns an image which has the thresholded values.  
+        """
         image[image < threshold] = 0
         image[image >= threshold] = 255
         return image
@@ -24,92 +45,146 @@ class coner():
     
 
     def min_eigen_trackbar(self,threshold):
-        # Apply cornerMinEigenVal
-        min_eigen = cv2.cornerMinEigenVal(self.orig_img,self.WINDOW_SIZE)
-        print(np.max(min_eigen))
-        self.threshold_eigen = self.binary_threshold(min_eigen,threshold/10000)
+        """This method is handling the entire process of edge detection which is then called by the trackbar
 
-        #Set all pixels that pass to 1 to find the passing corner values.
+        Args:
+            threshold (int): threshold value which determines the intensity of the value for binary_threshold method. 
+        """
+        # Use cornerMinEigenVal to find the minimum eignen values.
+        min_eigen = cv2.cornerMinEigenVal(self.orig_img,self.WINDOW_SIZE)
+        #print(np.max(min_eigen))
+    
+        # Threshold the result to obtain pixels which are greater than threshold/10000 
+        self.threshold_eigen = self.binary_threshold(deepcopy(min_eigen),threshold/10000)
+
+        # Set all pixels that pass to 1 to find the passing corner values.
         passing_edges = self.threshold_eigen/255
 
-        #Get cordinates of all passing edges
+        # Get cordinates of all passing edges to ignore all pixels which don't pass the conditions for non_maximum_supression
         non_max_list = np.argwhere(passing_edges > 0)
 
-        new_passed_photo = passing_edges * min_eigen
+        # Multiply the values in passing_edges to have an array containing only the values of the passing edges.
+        new_passed_photo = passing_edges *deepcopy(min_eigen) 
 
-        cords_list = self.non_maximum_suppression(new_passed_photo,non_max_list,filter_size=5)
+        # Apply non maximum supression which keeps only the brightest pixels in a 5 by 5 window.
+        cords_list = self.non_maximum_suppression(new_passed_photo,non_max_list,filter_size=self.WINDOW_SIZE)
+
+        # Add circles to the original image to show resulting corners
         circle_image = self.add_circles(cords_list)
-        print(len(cords_list))
+        #print(len(cords_list))
 
+
+
+        #Display the threshold result 
         cv2.imshow("Threshold Slider Window", self.threshold_eigen)
+        #Display the final image with circles added
         cv2.imshow("Final Non Max Circles", circle_image)
+        #Display the corner min eigen value window
+        cv2.imshow("cornerMinEigenVal Window",min_eigen)
+
+        if(self.save_image_once):
+            self.save_image_once = False
+            #Display the threshold result 
+            cv2.imwrite("Threshold_Slider.png", self.threshold_eigen)
+            #Display the final image with circles added
+            cv2.imwrite("Final_Non_Max_Circles.png", circle_image)
+            #Display the 
+            cv2.imwrite("cornerMinEigenVal.png",min_eigen)
 
 
-    # Method is responsbile for doing a blank type padding where it adds blank pixels to the edges of the image.
-    # Returns the padded image
     def pad_image(self,image,pad):
+        """Method is responsbile for doing a blank type padding where it adds blank pixels to the edges of the image.
+
+        Args:
+            image (numpy array): image that will be padded
+            pad (int): how many pixels of padding that will be added.
+
+        Returns:
+            numpy array: padded image
+        """
         padded_image = image
         for top in range(pad):
             top_bottom_pad = np.zeros(padded_image[0].shape)
-            # Use the top row to stack on the top of the image.
+            # Make blank array with length of the width to pad on top and bottom
             padded_image = np.vstack((top_bottom_pad,padded_image))
-            # Uses bottom row to stack on the bottom of the image
             padded_image = np.vstack((padded_image,top_bottom_pad))
         for rows in range(pad):
             left_right_pad = np.zeros(padded_image[:,0].shape)
-            # Use the left column to insert on the left.
+            # Make blank array with height of image to pad on the left and right
             padded_image= np.insert(padded_image,0,left_right_pad,axis=1)
-            # Use the right column to insert on the right.
             padded_image = np.insert(padded_image,padded_image.shape[1],left_right_pad,axis=1)
         return padded_image
 
     def add_circles(self,cords_list):
+        """Convert the image back into rgb and then adds circles to the image at the specified cords
+
+        Args:
+            cords_list (array[cords]): An array which stores cordinates for the passing corners.
+
+        Returns:
+            numpy array: returns a coloured image with circles drawn at corner points
+        """
+        # Change image to RGB
         cur_image = cv2.cvtColor(self.orig_img,cv2.COLOR_GRAY2RGB)
         for cord in cords_list:
+            # Draw cordinate with cv2.Circle
             cur_image = cv2.circle(cur_image,center=cord,radius=3,color=(0,255,0),thickness=-1)
 
         return cur_image
 
     def non_maximum_suppression(self,image,non_max_list, filter_size):
-        """_summary_
+        """This method iterates through a list of cordinates in the image and determines if the pixel in the center
+        is the greatest value. If it is it is added to a list for passing corner points, if not it is ignored.
 
         Args:
-            image (_type_): _description_
-            non_max_list (_type_): _description_
-            filter_size (_type_): _description_
+            image (numpy array): contains the image from new_passed_photo which are pixels that passed thresholding
+            non_max_list (array[cords[]]): contains the cordinates of the passing pixels
+            filter_size (int): the filter size which determines how big the window must for the non_maximum_supression
 
         Returns:
-            _type_: _description_
+            array[cords()]: returns a list of cordinates
         """
-        # Create the blank array for the convolution output.
-        conv_image = np.zeros(image.shape)
 
-        # Calculate the required amount of padding needed to keep the original dimensions of the image. 
+        # Calculate the required amount of padding needed to keep the original dimensions of the image
         pad = filter_size//2
 
-        # Doing so lets us optimize by not needing to check the pixel bounds every  
+        # Padding image the lets us optimize by not needing to check the image bounds.
         padded_image = self.pad_image(image,pad)
 
-        # Iterate through the image using the kernal 
+        # Set a blank array which will consist with the passing corner cords.
         brightest_centers = []
-        # Iterate through the padded image starting and stopping in the areas of the orignal image.
+
+        # Iterate through the non_max_list of pixels which passed the threshold.
         for cords in non_max_list:
+            # Obtain x and y value of non padded cordinates
             y = cords[0] + pad
             x = cords[1] + pad
+            # Cut out a portion of the image with a pad+1 by pad+1 size.
             non_max_filter = padded_image[y-pad :y + pad+1,x-pad :x + pad+1]
+
+            # Check the array for the index of the largest value
             max_index = np.unravel_index(np.argmax(non_max_filter, axis=None), non_max_filter.shape)
+
+            # if the largest index is (2,2) that means the center pixel is the largest and can pass the non maximum supression test.
             if(max_index == (pad,pad)):
+                # Append cordinates to brightest_centers array.
                 brightest_centers.append((x,y))
                             
-        # Return the conv image
+        # Return the non maximum supression corners 
         return brightest_centers
 
     def main(self):
+        """Main class responsible for calling the method which runs the entire process.
+        """
+        # Set a name for the trackbar slider window.
         cv2.namedWindow("Threshold Slider Window")
+
+        # Set initial values for the trackback 
         cv2.createTrackbar("Current", "Threshold Slider Window" ,self.INITIAL_SLIDER, self.MAX_SLIDER, self.min_eigen_trackbar)
         cv2.waitKey(0)
         
 
 if __name__ == "__main__":
+    # Construct and run main class.
     coner_class = coner()
     coner_class.main()
--- image_stitching/image_stitching.py
+++ image_stitching/image_stitching.py
@@ -1,12 +1,69 @@
+from pickletools import uint8
 import cv2
 import numpy as np
 
-
+def add_circles(image,cords_list):
+    cur_image = cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
+    for cord in cords_list:
+        c_cord=(int(cord[0][0]),int(cord[0][1]))
+        cur_image = cv2.circle(cur_image,center=c_cord,radius=3,color=(0,255,0),thickness=-1)
+    return cur_image
 
 def main():
-    pass
+    
+    base = cv2.imread("large2_uttower_left.jpg", cv2.IMREAD_GRAYSCALE)
+    trans = cv2.imread("uttower_right.jpg", cv2.IMREAD_GRAYSCALE)
 
+    detector = cv2.AKAZE_create()
 
+    # find the keypoints and descriptors with SIFT
+    kp_base, desc_base = detector.detectAndCompute(base, None)
+    kp_trans, desc_trans = detector.detectAndCompute(trans, None)
+
+    # create BFMatcher object
+    bf_match = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf_match.match(desc_trans,desc_base)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+    #print(matches)
+
+    trans_pts = np.float32([ kp_trans[matches[m].queryIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
+    base_pts = np.float32([ kp_base[matches[m].trainIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
+    #print(src_pts)
+    #print(dst_pts)
+
+    # Draw matching points on both images.
+    trans_circle = add_circles(trans,trans_pts)
+    base_circle = add_circles(base,base_pts)
+
+    cv2.imwrite("Transform_Image_Matching_Points.png",trans_circle)
+    cv2.imwrite("Base_Image_Matching_Points.png",base_circle)
+    cv2.imshow('Transform Image', trans_circle)
+    cv2.imshow('Base Image', base_circle)
+    
+    # Compute Homeography by using the trans_pts and base_pts.
+    trans_matrix, _ = cv2.findHomography(trans_pts,base_pts,cv2.RANSAC) 
+    #print(trans_matrix)
+
+    #Prefrom warped perspective using the transformation matrix on the trans image. 
+    #Use the base.shape so that it has the same matrix size as the other.
+    trans_warp = cv2.warpPerspective(trans, trans_matrix, (base.shape[1], base.shape[0]))
+    cv2.imshow("Transform Warp", trans_warp)
+    cv2.imwrite("Transform_Warp.png", trans_warp)
+    
+    #Merge the images using the or wise method. 
+    merge_image = base | trans_warp
+    cv2.imshow("Merged Image", merge_image)
+    cv2.imwrite("Merged_Image.png", merge_image)
+
+    cv2.imshow("Merged Image", merge_image)
+    cv2.imwrite("Merged_Image.png", merge_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
